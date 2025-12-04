f930313b120709c7c1436a006fcb0e26f8bb4659
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2025-12-04 11:20:50 -0800

    Update prxxry.config.js

--- prxxry.config.js
+++ prxxry.config.js
@@ -2,7 +2,7 @@
 const basePath = '/Staticsj/';
 
 // Match the default in script.js
-const DEFAULT_WISP = "wss://dash.goip.de/wisp/"; 
+const DEFAULT_WISP = "wss://wisp.rhw.one/wisp/"; 
 
 let _CONFIG = {
   wispurl: localStorage.getItem("proxServer") || DEFAULT_WISP,
